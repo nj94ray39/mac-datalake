27e36442f7f9288c8a00409959ea0f22bdab1007
nj94ray39 <nj94.ray39@gmail.com>
2020-03-15 17:27:16 +0530

    First commit

---
+++ scripts/01bake/0100_all_start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+####################################################
+#
+#	Author:
+#		BeCenturion Group
+#
+#	Description:
+#		The script marks the start of bake stage.
+#
+#	Usage:
+#		bash 0100_all_start.sh
+#	Changes:
+#		1.0 15-March-2020 Nihal Jangave
+#
+####################################################
+
+#Exit if an error is encountered.
+set -e
+
+echo "Start stage"
+
+echo "Done..."
